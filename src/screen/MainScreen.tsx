af297b63c2607e68bea9de2aba6a8e652d9966ab
MicPhumin <114295792+MicPhumin@users.noreply.github.com>
2026-08-06 14:14:28 +0700

    update restart function and change topic func

--- src/screen/MainScreen.tsx
+++ src/screen/MainScreen.tsx
@@ -15,7 +15,12 @@ import {
 import { useEffect, useState } from "react";
 import { DndContext, closestCenter } from "@dnd-kit/core";
 import { supabase } from "../supabase";
-import { AiFillAlert, AiOutlineReload } from "react-icons/ai";
+import {
+  AiFillAlert,
+  AiFillCheckCircle,
+  AiFillPlusSquare,
+  AiOutlineReload,
+} from "react-icons/ai";
 
 import {
   SortableContext,
@@ -46,6 +51,7 @@ const MainScreen = () => {
   const [showVal, setShowVal] = useState<boolean>(false);
   const [name, setName] = useState("");
   const [topic, setTopic] = useState("");
+  const [changeTopic, setChangeTopic] = useState<boolean>(false);
   const [score, setScore] = useState(0);
   const [hostBtn, setHostBtn] = useState(false);
   const [isHost, setIsHost] = useState<SortableCardProps>({
@@ -94,6 +100,7 @@ const MainScreen = () => {
           return item.id == player?.id;
         });
         setMyCards(findPlayer);
+        localStorage.setItem("player", JSON.stringify(findPlayer));
         setIsModalOpen(false);
       } else if (findHost.id !== player.id) {
         localStorage.clear();
@@ -137,8 +144,16 @@ const MainScreen = () => {
             setScore(player.score);
 
             if (player.showVal && player.showVal === true) {
+              console.log("ShowVal");
               setShowVal(player.showVal);
               setIsNewGame(player.showVal);
+            } else if (player.showVal === false) {
+              setShowVal(false);
+              setIsNewGame(false);
+            }
+
+            if (player.topic) {
+              setTopic(player.topic);
             }
           }
         },
@@ -153,30 +168,39 @@ const MainScreen = () => {
     };
   }, []);
 
-  // const handleRestart = async () => {
-  //   const shuffled = Array.from({ length: 100 }, (_, i) => i + 1);
+  const handleRestart = async () => {
+    const shuffled = Array.from({ length: 100 }, (_, i) => i + 1);
 
-  //   for (let i = shuffled.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  //   }
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
 
-  //   for (let i = 0; i < cards.length; i++) {
-  //     const randomNumber = shuffled.pop();
+    for (let i = 0; i < cards.length; i++) {
+      const randomNumber = shuffled.pop();
+      await supabase
+        .from("itomic")
+        .update({
+          value: Number(randomNumber),
+          active: null,
+          score: null,
+          showVal: false,
+        })
+        .eq("id", cards[i].id);
+    }
+  };
 
-  //     console.log("randomNumber", randomNumber);
-  //     console.log("cards[i]", cards[i]);
-  //     const { data, error } = await supabase
-  //       .from("itomic")
-  //       .update({
-  //         value: Number(randomNumber),
-  //         active: null,
-  //         score: null,
-  //         showVal: false,
-  //       })
-  //       .eq("id", cards[i].id);
-  //   }
-  // };
+  const handleTopic = async (topicName: string) => {
+    const host = cards.find((item) => {
+      return item.is_host === true;
+    });
+    await supabase
+      .from("itomic")
+      .update({ topic: topicName })
+      .eq("id", host?.id);
+
+    setChangeTopic(false);
+  };
 
   const handleRandomTopic = () => {
     const index = Math.floor(Math.random() * topicGame.length);
@@ -393,7 +417,7 @@ const MainScreen = () => {
                 />{" "}
                 <Button
                   variant="solid"
-                  color="green"
+                  color="purple"
                   onClick={() => {
                     handleRandomTopic();
                   }}
@@ -484,9 +508,70 @@ const MainScreen = () => {
         </Col>
 
         <Col xs={24} sm={24} md={20} lg={16} xl={18}>
-          <Row>
-            <h2 style={{ fontSize: "30px" }}>{topic}</h2>
-          </Row>
+          {changeTopic === true ? (
+            <>
+              <Row gutter={4}>
+                <Col xs={16} sm={20} md={16} lg={16} xl={16}>
+                  {" "}
+                  <Input
+                    placeholder="Enter Topic"
+                    size="large"
+                    value={topic}
+                    onChange={(e) => {
+                      setTopic(e.target.value);
+                    }}
+                  />{" "}
+                </Col>
+                <Col xs={4} sm={4} md={4} lg={4} xl={4}>
+                  <Button
+                    variant="solid"
+                    color="purple"
+                    size="large"
+                    onClick={() => {
+                      handleRandomTopic();
+                    }}
+                    icon={<AiFillAlert />}
+                    style={{ marginRight: "10px" }}
+                  >
+                    Random
+                  </Button>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: "10px" }}>
+                <Button
+                  variant="solid"
+                  size="large"
+                  color="green"
+                  onClick={() => {
+                    handleTopic(topic);
+                  }}
+                  icon={<AiFillCheckCircle />}
+                  style={{ marginRight: "10px" }}
+                >
+                  ok
+                </Button>
+              </Row>
+            </>
+          ) : (
+            <>
+              <Row>
+                <h2 style={{ fontSize: "30px" }}>{topic}</h2>{" "}
+              </Row>
+              <Row>
+                <Button
+                  variant="solid"
+                  color="purple"
+                  onClick={() => {
+                    setChangeTopic(true);
+                  }}
+                  icon={<AiFillPlusSquare />}
+                  style={{ marginRight: "10px" }}
+                >
+                  Change Topic
+                </Button>
+              </Row>
+            </>
+          )}
         </Col>
 
         <Col xs={20} sm={20} md={10} lg={4} xl={4}>
@@ -545,7 +630,10 @@ const MainScreen = () => {
                   <Row
                     gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
                     align={"middle"}
-                    style={{ pointerEvents: isNewGame ? "none" : "auto" }}
+                    style={{
+                      zIndex: -1,
+                      // pointerEvents: isNewGame ? "none" : "auto",
+                    }}
                   >
                     {cards.map((card, index) => (
                       <>
@@ -591,13 +679,14 @@ const MainScreen = () => {
             <Row justify={"center"}>
               <Button
                 variant="solid"
-                color="purple"
+                color="green"
                 onClick={() => {
                   handleOrder();
                 }}
+                icon={<AiFillCheckCircle />}
                 style={{
-                  fontSize: "30px",
-                  width: "300px",
+                  fontSize: "25px",
+                  width: "200px",
                   height: "50px",
                 }}
               >
@@ -606,57 +695,53 @@ const MainScreen = () => {
             </Row>
           ) : (
             <>
-              <Row justify={"center"}>
-                <Button
-                  variant="solid"
-                  color="green"
-                  onClick={() => {
-                    deleteAllRows();
-                  }}
-                  style={{
-                    fontSize: "30px",
-                    width: "300px",
-                    height: "50px",
-                  }}
-                >
-                  New Game
-                </Button>
+              <Row justify={"center"} gutter={24}>
+                <Col>
+                  {" "}
+                  <Button
+                    variant="solid"
+                    color="red"
+                    onClick={() => {
+                      handleRestart();
+                    }}
+                    icon={<AiOutlineReload />}
+                    style={{
+                      fontSize: "25px",
+                      width: "200px",
+                      height: "50px",
+                    }}
+                  >
+                    Restart
+                  </Button>
+                </Col>
+                <Col>
+                  {" "}
+                  <Row justify={"center"}>
+                    <Button
+                      variant="solid"
+                      color="purple"
+                      onClick={() => {
+                        deleteAllRows();
+                      }}
+                      icon={<AiFillPlusSquare />}
+                      style={{
+                        fontSize: "25px",
+                        width: "200px",
+                        height: "50px",
+                      }}
+                    >
+                      New Game
+                    </Button>
+                  </Row>
+                </Col>
               </Row>
             </>
           )}
-          {/* <Button
-            variant="solid"
-            color="purple"
-            onClick={() => {
-              handleOrder();
-            }}
-            style={{
-              fontSize: "30px",
-              width: "300px",
-              height: "50px",
-            }}
-          >
-            Finish2
-          </Button>
-          <Button
-            variant="solid"
-            color="purple"
-            onClick={() => {
-              handleRestart();
-            }}
-            style={{
-              fontSize: "30px",
-              width: "300px",
-              height: "50px",
-            }}
-          >
-            Generate Num
-          </Button> */}
         </>
       )}
 
       <Row justify={"end"}>
-        <h3 style={{ fontSize: "20px", color: "magenta" }}>iTOMIC ver 1.7.0</h3>
+        <h3 style={{ fontSize: "20px", color: "magenta" }}>iTOMIC ver 1.8.0</h3>
       </Row>
     </div>
   );
