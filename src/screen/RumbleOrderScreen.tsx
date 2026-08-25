e5301c9ae2e212260649696ef3956619647db604
MicPhumin <114295792+MicPhumin@users.noreply.github.com>
2026-08-25 18:05:36 +0700

    Update RumbleOrderScreen.tsx

--- src/screen/RumbleOrderScreen.tsx
+++ src/screen/RumbleOrderScreen.tsx
@@ -737,6 +737,7 @@ const RumbleOrderScreen = () => {
                             topic={card.topic}
                             note={card.note}
                             noteColor={card.notecolor}
+                            host={myCards?.is_host}
                           />
                         </Col>
                       </>
